f347771792dfbcbe6b52bbb74980d62218648bf4
aibolscorpion <AI57451298ai>
2021-10-04 20:08:33 +0600

    7 commit

--- app/src/main/java/kz/almaty/sadaqa/view/PeopleInNeedListAdapter.kt
+++ app/src/main/java/kz/almaty/sadaqa/view/PeopleInNeedListAdapter.kt
@@ -3,22 +3,15 @@ package kz.almaty.sadaqa.view
 import android.content.Context
 import android.content.Intent
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import kz.almaty.sadaqa.R
+import kz.almaty.sadaqa.databinding.ItemRecyclerviewBinding
 import kz.almaty.sadaqa.model.Person
 
 class PeopleInNeedListAdapter(val context : Context, val peopleList : List<Person>) : RecyclerView.Adapter<PeopleInNeedListAdapter.PersonViewHolder>(){
-    class PersonViewHolder(context : Context, itemView : View): RecyclerView.ViewHolder(itemView){
-        var nameTV : TextView ?= null
-        var personIV : ImageView
+    class PersonViewHolder(context : Context,val binding : ItemRecyclerviewBinding): RecyclerView.ViewHolder(binding.root){
         init {
-            personIV = itemView.findViewById(R.id.peopleInNeedPhoto)
-            nameTV = itemView.findViewById(R.id.nameTV)
             itemView.setOnClickListener{
                 val intent = Intent(context, PersonProfileActivity::class.java)
                 intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
@@ -28,14 +21,13 @@ class PeopleInNeedListAdapter(val context : Context, val peopleList : List<Perso
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PersonViewHolder {
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_recyclerview, parent, false)
-        return PersonViewHolder(context, itemView)
+        return PersonViewHolder(context, ItemRecyclerviewBinding.inflate(LayoutInflater.from(parent.context), parent, false))
     }
 
     override fun onBindViewHolder(holder: PersonViewHolder, position: Int) {
         val person = peopleList[position]
-        holder.nameTV?.text = person.name
-        Glide.with(context).load(person.photo).into(holder.personIV)
+        holder.binding.person = person
+        Glide.with(context).load(person.photo).into(holder.binding.peopleInNeedPhoto)
 
     }
 
